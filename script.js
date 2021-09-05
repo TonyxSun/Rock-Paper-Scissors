e3af8d02e042c78e7a5f871a46fb32fe2fbf69e2
Tony Sun <sunwuyue@live.com>
2021-09-04 22:19:42 -0700

    Typing text effect and JS fixes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerScore = 0;
 let playerSelection = "";
 let computerSelection = "";
 let roundNumber = 0;
+let introFinished = false;
 const introClick = document.querySelector('#click-continue');
 const gameInterface = document.querySelector('#game-container');
 const resultsDiv = document.querySelector('.results');
@@ -15,10 +16,32 @@ const endMsg = document.querySelector('#end-msg');
 const btns = document.querySelectorAll('.game-option');
 const retryBtn = document.querySelector('#retry-btn');
 
-function start(){
-    introClick.classList.add('hidden');
-    gameInterface.classList.toggle('hidden');
-    window.removeEventListener('click',start);
+let i = 0;
+let text = `Your system has been taken over by EvilBot 3.14. 
+        The only way to salvage your files and defeat the evil AI 
+        is to play a game even the mastermind hasn't yet figured out...`;
+
+function intro() {
+    if (i < text.length) {
+        document.getElementById('intro').innerHTML += text.charAt(i);
+        i++;
+        if (i == text.length) {
+            setTimeout(intro, 1000);
+        } else {
+            setTimeout(intro, 50);
+        }
+    } else {
+        introFinished = true;
+        introClick.classList.toggle('hidden');
+    }
+}
+
+function startgame() {
+    if (introFinished == true) {
+        introClick.classList.toggle('hidden');
+        gameInterface.classList.toggle('hidden');
+        window.removeEventListener('click', startgame);
+    }
 }
 
 function computerPlay() {
@@ -32,7 +55,7 @@ function playRound(e) {
     //determines the winner of one round of RockPaper Scissors with user and computer imputs
     playerSelection = this.id;
     computerSelection = computerPlay();
-    roundNumber ++;
+    roundNumber++;
     if (playerSelection == "rock") {
         printRound(computerSelection == "scissors"
             ? ++playerScore && "You win! Rock beats scissors."
@@ -41,7 +64,7 @@ function playRound(e) {
         printRound(computerSelection == "paper"
             ? ++playerScore && "You win! Scissor beats paper."
             : (computerSelection == "rock") ? ++computerScore && "You lost to rock." : "Tied!");
-    } else if (playerSelection == "paper"){
+    } else if (playerSelection == "paper") {
         printRound(computerSelection == "rock"
             ? ++playerScore && "You win! Paper beats rock."
             : (computerSelection == "scissors") ? ++computerScore && "You lost to scissors." : "Tied!");
@@ -50,7 +73,7 @@ function playRound(e) {
 
 let printRound = function (msg) {
     // prints results of current round 
-    roundResults.innerHTML = `Round ${roundNumber}: You chose: ${playerSelection}. EvilBot chose: ${computerSelection}.\n <strong>${msg}</strong>`;
+    roundResults.innerHTML = `Round ${roundNumber}: You chose: ${playerSelection}. EvilBot chose: ${computerSelection}.<br> <strong>${msg}</strong>`;
     resultsDiv.insertBefore(roundResults, scoresDiv);
     playerResults.textContent = `Player: ${playerScore}`;
     computerResults.textContent = `EvilBot: ${computerScore}`
@@ -63,10 +86,10 @@ let printRound = function (msg) {
 let printWinner = function () {
     // prints winner
     let msg;
-    if (playerScore > computerScore){
+    if (playerScore > computerScore) {
         msg = "You won! You have defeated EvilBot 3.14 and it has left you alone - HOORAY!!!\n";
     } else {
-        msg = "Sorry, you lost. You will find your hard drive filled with pictures of 'pie' when you wake up tomorrow. Condolences.\n";
+        msg = "Sorry, you lost. You will find your hard drive filled with pictures of pies when you wake up tomorrow. Condolences.\n";
     }
     endMsg.innerHTML = msg;
     gameInterface.classList.toggle('hidden');
@@ -74,9 +97,10 @@ let printWinner = function () {
 };
 
 
-function restart(){
+function restart() {
     playerScore = 0;
     computerScore = 0;
+    introFinished = false;
     roundResults.innerHTML = '';
     playerResults.textContent = `Player: ${playerScore}`;
     computerResults.textContent = `Computer: ${computerScore}`
@@ -85,7 +109,8 @@ function restart(){
     endDiv.classList.toggle('hidden');
 }
 
-window.addEventListener('click',start);
+intro()
+window.addEventListener('click', startgame);
 btns.forEach(btn => btn.addEventListener('click', playRound));
 retryBtn.addEventListener('click', restart);
 
