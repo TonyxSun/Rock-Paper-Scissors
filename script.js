7a7afc2463161ad8d65fbaac156473b832d194fc
Tony Sun <sunwuyue@live.com>
2021-09-04 18:50:48 -0700

    Game function complete/updated

--- script.js
+++ script.js
@@ -10,74 +10,67 @@ function computerPlay() {
     return options[selector];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
     //determines the winner of one round of RockPaper Scissors with user and computer imputs
-    playerSelection = playerSelection.toLowerCase();
-    if (!checkInput(playerSelection)) {
+    playerSelection = this.id;
+    computerSelection = computerPlay();
+
+    if (playerSelection == "rock") {
+        printRound(computerSelection == "scissors"
+            ? ++playerScore && "You win! Rock beats scissors."
+            : (computerSelection == "paper") ? ++computerScore && "You lost to paper :(." : "Tied!");
+    } else if (playerSelection == "scissors") {
+        printRound(computerSelection == "paper"
+            ? ++playerScore && "You win! Scissor cuts paper."
+            : (computerSelection == "rock") ? ++computerScore && "You lost to rock :(." : "Tied!");
+    } else if (playerSelection == "paper"){
+        printRound(computerSelection == "rock"
+            ? ++playerScore && "You win! Paper beats rock."
+            : (computerSelection == "scissors") ? ++computerScore && "You lost to scissors :(." : "Tied!");
+    }
+}
+
+let printRound = function (msg) {
+    // prints results of current round 
+    roundResults.innerHTML = `Player chose: ${playerSelection}. Computer chose: ${computerSelection}.\n <strong>${msg}</strong>`;
+    resultsDiv.insertBefore(roundResults, scoresDiv);
+    playerResults.textContent = `Player: ${playerScore}`;
+    computerResults.textContent = `Computer: ${computerScore}`
+    if (playerScore == 5 || computerScore == 5) {
+        printWinner();
         return;
     }
+};
 
-    if (playerSelection == computerSelection) {
-        return "Tied!";
-    }
-    if (playerSelection == "rock") {
-        return computerSelection == "scissors"
-            ? ++playerScore && "You win! Rock beats scissors."
-            : ++computerScore && "You lost to paper :(.";
-    } else if (playerSelection == "scissors") {
-        return computerSelection == "paper"
-            ? ++playerScore && "You win! Scissor cuts paper."
-            : ++computerScore && "You lost to rock :(.";
+let printWinner = function () {
+    // prints winner
+    let msg;
+    if (playerScore > computerScore){
+        msg = "You won! You have defeated your opponent!!!\n";
     } else {
-        return computerSelection == "rock"
-            ? ++playerScore && "You win! Paper beats rock."
-            : ++computerScore && "You lost to scissors :(.";
+        msg = "Sorry, you lost. Better luck next time!\n";
     }
+    endMsg.innerHTML = msg;
+};
+
+function restart(){
+    playerScore = 0;
+    computerScore = 0;
+    roundResults.innerHTML = '';
+    playerResults.textContent = `Player: ${playerScore}`;
+    computerResults.textContent = `Computer: ${computerScore}`
+    endMsg.textContent = '';
 }
 
-let printChoice = function (playerSelection, computerSelection) {
-    return (
-        "Player chose: " +
-        playerSelection +
-        ". Computer chose: " +
-        computerSelection +
-        "."
-    );
-};
+const resultsDiv = document.querySelector('.results');
+const scoresDiv = document.querySelector('#scores');
+const roundResults = document.querySelector('#round-results');
+const playerResults = document.querySelector('#player-score');
+const computerResults = document.querySelector('#computer-score');
+const endDiv = document.querySelector('#game-end');
+const endMsg = document.querySelector('#end-msg');
+const btns = document.querySelectorAll('.game-option');
+const retryBtn = document.querySelector('#retry-btn');
+btns.forEach(btn => btn.addEventListener('click', playRound));
+retryBtn.addEventListener('click', restart);
 
-let printScore = function () {
-    // prints current score
-    return "Score is " + playerScore + " to " + computerScore + ".";
-};
-
-let checkInput = function (input) {
-    if (input != "rock" && input != "scissors" && input != "paper") {
-        alert("Please enter a valid value.");
-        return false;
-    }
-    return true;
-};
-
-function game() {
-    // plays up to 20 games or first to 5.
-    for (let i = 0; i < 20; i++) {
-        playerSelection = prompt(
-            "What is your choice? Choose from 'rock', 'paper' or 'scissors' verbatim: "
-        );
-        computerSelection = computerPlay();
-        alert(
-            playRound(playerSelection, computerSelection) +
-            " " +
-            printChoice(playerSelection, computerSelection) +
-            " " +
-            printScore()
-        );
-        if (playerScore == 5 || computerScore == 5) {
-            break;
-        }
-    }
-    alert(playerScore > computerScore ? "You win!" : "You lose!");
-    printScore();
-}
-
-game();
