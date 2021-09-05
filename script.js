de8b4c2f78a3988eeb9451871ce5950e09bd8956
Tony Sun <sunwuyue@live.com>
2021-09-04 21:18:39 -0700

    Finish css

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let playerScore = 0;
 let computerScore = 0;
 let playerSelection = "";
 let computerSelection = "";
+let roundNumber = 0;
+const introClick = document.querySelector('#click-continue');
+const gameInterface = document.querySelector('#game-container');
+const resultsDiv = document.querySelector('.results');
+const scoresDiv = document.querySelector('#scores');
+const roundResults = document.querySelector('#round-results');
+const playerResults = document.querySelector('#player-score');
+const computerResults = document.querySelector('#computer-score');
+const endDiv = document.querySelector('#end-container');
+const endMsg = document.querySelector('#end-msg');
+const btns = document.querySelectorAll('.game-option');
+const retryBtn = document.querySelector('#retry-btn');
+
+function start(){
+    introClick.classList.add('hidden');
+    gameInterface.classList.toggle('hidden');
+    window.removeEventListener('click',start);
+}
 
 function computerPlay() {
     //computer makes a random choice between R/P/S
@@ -14,28 +32,28 @@ function playRound(e) {
     //determines the winner of one round of RockPaper Scissors with user and computer imputs
     playerSelection = this.id;
     computerSelection = computerPlay();
-
+    roundNumber ++;
     if (playerSelection == "rock") {
         printRound(computerSelection == "scissors"
             ? ++playerScore && "You win! Rock beats scissors."
-            : (computerSelection == "paper") ? ++computerScore && "You lost to paper :(." : "Tied!");
+            : (computerSelection == "paper") ? ++computerScore && "You lost to paper." : "Tied!");
     } else if (playerSelection == "scissors") {
         printRound(computerSelection == "paper"
-            ? ++playerScore && "You win! Scissor cuts paper."
-            : (computerSelection == "rock") ? ++computerScore && "You lost to rock :(." : "Tied!");
+            ? ++playerScore && "You win! Scissor beats paper."
+            : (computerSelection == "rock") ? ++computerScore && "You lost to rock." : "Tied!");
     } else if (playerSelection == "paper"){
         printRound(computerSelection == "rock"
             ? ++playerScore && "You win! Paper beats rock."
-            : (computerSelection == "scissors") ? ++computerScore && "You lost to scissors :(." : "Tied!");
+            : (computerSelection == "scissors") ? ++computerScore && "You lost to scissors." : "Tied!");
     }
 }
 
 let printRound = function (msg) {
     // prints results of current round 
-    roundResults.innerHTML = `Player chose: ${playerSelection}. Computer chose: ${computerSelection}.\n <strong>${msg}</strong>`;
+    roundResults.innerHTML = `Round ${roundNumber}: You chose: ${playerSelection}. EvilBot chose: ${computerSelection}.\n <strong>${msg}</strong>`;
     resultsDiv.insertBefore(roundResults, scoresDiv);
     playerResults.textContent = `Player: ${playerScore}`;
-    computerResults.textContent = `Computer: ${computerScore}`
+    computerResults.textContent = `EvilBot: ${computerScore}`
     if (playerScore == 5 || computerScore == 5) {
         printWinner();
         return;
@@ -46,12 +64,15 @@ let printWinner = function () {
     // prints winner
     let msg;
     if (playerScore > computerScore){
-        msg = "You won! You have defeated your opponent!!!\n";
+        msg = "You won! You have defeated EvilBot 3.14 and it has left you alone - HOORAY!!!\n";
     } else {
-        msg = "Sorry, you lost. Better luck next time!\n";
+        msg = "Sorry, you lost. You will find your hard drive filled with pictures of 'pie' when you wake up tomorrow. Condolences.\n";
     }
     endMsg.innerHTML = msg;
+    gameInterface.classList.toggle('hidden');
+    endDiv.classList.toggle('hidden');
 };
+
 
 function restart(){
     playerScore = 0;
@@ -60,17 +81,11 @@ function restart(){
     playerResults.textContent = `Player: ${playerScore}`;
     computerResults.textContent = `Computer: ${computerScore}`
     endMsg.textContent = '';
+    gameInterface.classList.toggle('hidden');
+    endDiv.classList.toggle('hidden');
 }
 
-const resultsDiv = document.querySelector('.results');
-const scoresDiv = document.querySelector('#scores');
-const roundResults = document.querySelector('#round-results');
-const playerResults = document.querySelector('#player-score');
-const computerResults = document.querySelector('#computer-score');
-const endDiv = document.querySelector('#game-end');
-const endMsg = document.querySelector('#end-msg');
-const btns = document.querySelectorAll('.game-option');
-const retryBtn = document.querySelector('#retry-btn');
+window.addEventListener('click',start);
 btns.forEach(btn => btn.addEventListener('click', playRound));
 retryBtn.addEventListener('click', restart);
 
